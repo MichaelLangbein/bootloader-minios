078fa5f8c5ad78866f775ff46a3f4d854a7c0793
Michael Langbein <michael_langbein@hotmail.de>
2023-05-06 00:45:22 +0200

    printString as legit function

--- 0_wasm_experiments/boot_sect.asm
+++ 0_wasm_experiments/boot_sect.asm
@@ -1,20 +1,33 @@
 [org 0x7c00]
 
 mov bx, greeting        ; this works with bx, but not with bh
-printString:
-    mov ah, 0x0e
-    mov al, [bx]
-    cmp al, 0
-    je loop
-    int 0x10
-    add bx, 1
-    jmp printString
+call printString
+
+mov bx, goodbye
+call printString
 
 loop:
     jmp loop
 
+printString:
+    pusha
+    mov ah, 0x0e
+    printChar:
+        mov al, [bx]
+        cmp al, 0
+        je exitPrintString
+        int 0x10
+        add bx, 1
+        jmp printChar
+    exitPrintString:
+        popa
+        ret
+
 greeting:
     db 'Hello, world!',0
+
+goodbye:
+    db 'Goodbye!',0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
