f0a2d1149f2e72eb3ca0b8567c6f5ae3a727b1e1
MichaelLangbein <michael_langbein@hotmail.de>
2023-05-06 21:16:49 +0200

    compiles and runs

--- arm/kernel.c
+++ arm/kernel.c
@@ -1,6 +1,16 @@
 #include <stddef.h>
 #include <stdint.h>
 
+
+/**
+ * MMIO:
+ * Memory mapped io.
+ * Peripherals get a portion of memory at a certain offset from peripheral-base-address at 0x0x3F000000
+ * 4-byte parts of that portion are called `registers`. 
+ * Usually at least on register is a control-register.
+ * Commonly another one is a write-register.
+ */
+
 static inline void mmio_write(uint32_t reg, uint32_t data) {
     *(volatile uint32_t*)reg = data;
 }
@@ -45,9 +55,14 @@ enum {
     UART0_TDR    = (UART0_BASE + 0x8C),
 };
 
+/**
+ * Sets some common configuration-flags for various registers.
+*/
 void uart_init() {
+    // disable uart
     mmio_write(UART0_CR, 0x00000000);
 
+    // disabling gpio-pints 14 and 15.
     mmio_write(GPPUD, 0x00000000);
     delay(150);
 
@@ -56,19 +71,31 @@ void uart_init() {
 
     mmio_write(GPPUDCLK0, 0x00000000);
 
+    // clear all pending interrupts
     mmio_write(UART0_ICR, 0x7FF);
 
+    // sets a baud-rate
     mmio_write(UART0_IBRD, 1);
     mmio_write(UART0_FBRD, 40);
 
+    // writes bits 4, 5, 6 to line-control-register.
+    // 4: uart holds an 8 item deep fifo
+    // 5, 6: data has 8-bit long words
     mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
 
+    // writes ones to interrupt-mask: disabling all interrupts from uart.
     mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
             (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
 
+    // writes bits 0, 8, 9 to control-register
+    // 0: enables uart-hardware
+    // 8: enables receiving data
+    // 9: enables transmitting data
     mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
 }
 
+// FR: flag-register: tells if there's data to read
+// DR: data-register: where data is written to and read from.
 void uart_putc(unsigned char c) {
     while ( mmio_read(UART0_FR) & (1 << 5) ) { }
     mmio_write(UART0_DR, c);
@@ -84,9 +111,14 @@ void uart_puts(const char* str) {
         uart_putc((unsigned char)str[i]);
 }
 
+
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
+    // In ARM, first three arguments are passed through registers r0, r1, and r2.
+
+    // r0 and r1 are parameters by convention, but not used here.
     (void) r0;
     (void) r1;
+    
     (void) atags;
 
     uart_init();
