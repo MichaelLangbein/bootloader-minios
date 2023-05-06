2fad93ccce72525cee90c1b3c817e7bfe428448f
MichaelLangbein <michael_langbein@hotmail.de>
2023-05-06 21:31:39 +0200

    nice makefile and dir-structure

--- arm/compile.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# fpic: create position-independent code.
-# ffreestanding: no libc available at runtime; may not have main function.
-# nostdlib: no stdlib available at runtime.
-
-arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -c boot.S -o boot.o
-arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -std=gnu99 -c kernel.c -o kernel.o -O2 -Wall -Wextra
-arm-none-eabi-gcc -T linker.ld -o myos.elf -ffreestanding -O2 -nostdlib boot.o kernel.o
